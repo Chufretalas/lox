c86289fef1811c4ed36f0345fa84b950fa6e2038
Chufretalas <101844490+Chufretalas@users.noreply.github.com>
2026-03-07 18:37:35 -0300

    Chapter 19

--- clox/vm.h
+++ clox/vm.h
@@ -7,23 +7,25 @@
 #define STACK_MAX 4096
 
 typedef struct {
-  Chunk* chunk;
-  uint8_t* ip; // Instruction pointer
-  Value stack[STACK_MAX];
-  Value* stackTop;
+    Chunk *chunk;
+    uint8_t *ip; // Instruction pointer
+    Value stack[STACK_MAX];
+    Value *stackTop;
+    Obj *objects;
 } VM;
 
 typedef enum {
-  INTERPRET_OK,
-  INTERPRET_COMPILE_ERROR,
-  INTERPRET_RUNTIME_ERROR
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(const char* source);
+InterpretResult interpret(const char *source);
 void push(Value value);
 Value pop();
-
 
 #endif
