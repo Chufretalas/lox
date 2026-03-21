fbfb75af1c7dc7b5e0568977a779da1f7a92f915
Chufretalas <101844490+Chufretalas@users.noreply.github.com>
2026-03-21 19:29:51 -0300

    done chapter 21

--- clox/vm.c
+++ clox/vm.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
@@ -29,10 +30,13 @@ static void runtimeError(const char *format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM() {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -73,8 +77,9 @@ static InterpretResult run() {
 #define READ_CONSTANT_FROM(index) (vm.chunk->constants.values[index])
 #define READ_24BIT()                                                           \
     (vm.ip += 3, (uint32_t)(vm.ip[-3] | (vm.ip[-2] << 8) | (vm.ip[-1] << 16)))
+#define READ_STRING() AS_STRING(READ_CONSTANT())
+#define READ_STRING_LONG() AS_STRING(READ_CONSTANT_FROM(READ_24BIT()))
     // Modifying the ninary result in place to avoid a pop and push
-
 #define BINARY_OP(valueType, op)                                               \
     do {                                                                       \
         if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) {                      \
@@ -119,6 +124,59 @@ static InterpretResult run() {
         case OP_FALSE:
             push(BOOL_VAL(false));
             break;
+        case OP_POP:
+            pop();
+            break;
+        case OP_GET_GLOBAL: {
+            ObjString *name = READ_STRING();
+            Value value;
+            if (!tableGet(&vm.globals, name, &value)) {
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(value);
+            break;
+        }
+        case OP_GET_GLOBAL_LONG: {
+            ObjString *name = READ_STRING_LONG();
+            Value value;
+            if (!tableGet(&vm.globals, name, &value)) {
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(value);
+            break;
+        }
+        case OP_DEFINE_GLOBAL: {
+            ObjString *name = READ_STRING();
+            tableSet(&vm.globals, name, peek(0));
+            pop();
+            break;
+        }
+        case OP_DEFINE_GLOBAL_LONG: {
+            ObjString *name = READ_STRING_LONG();
+            tableSet(&vm.globals, name, peek(0));
+            pop();
+            break;
+        }
+        case OP_SET_GLOBAL: {
+            ObjString *name = READ_STRING();
+            if (tableSet(&vm.globals, name, peek(0))) {
+                tableDelete(&vm.globals, name);
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            break;
+        }
+        case OP_SET_GLOBAL_LONG: {
+            ObjString *name = READ_STRING_LONG();
+            if (tableSet(&vm.globals, name, peek(0))) {
+                tableDelete(&vm.globals, name);
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            break;
+        }
         case OP_EQUAL: {
             Value b = pop();
             Value a = pop();
@@ -163,9 +221,13 @@ static InterpretResult run() {
             }
             push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
-        case OP_RETURN: {
+        case OP_PRINT: {
             printValue(pop());
             printf("\n");
+            break;
+        }
+        case OP_RETURN: {
+            // Exit interpreter.
             return INTERPRET_OK;
         }
         }
@@ -175,6 +237,8 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 #undef READ_CONSTANT_FROM
 #undef READ_24BIT
+#undef READ_STRING
+#undef READ_STRING_LONG
 #undef BINARY_OP
 }
 
